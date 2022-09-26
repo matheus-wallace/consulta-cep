2ce6466536c1dc9f2ef3d2bf28847fa1fffd1907
Matheus Wallace da Silva Pereira <ma.wallace1@hotmail.com>
2022-09-25 23:12:48 -0300

    feat: new feature to create or remove inputs and limit the quantity to 5 request

---
+++ js/inputGenerator.js
@@ -0,0 +1,33 @@
+const newInputCEP = document.querySelector(".form__createInput")
+const inputContianer = document.querySelector(".form__inputContainer")
+
+function createNewInput(){
+  if (inputContianer.children.length < 9) {
+    const input = document.createElement("input")
+    input.className = "form__inputCEP"
+    input.setAttribute("placeholder", "00.000-000")
+    input.setAttribute("data-mask","00.000-000")
+    inputContianer.appendChild(input)
+    const removeInput = document.createElement("button")
+    removeInput.className = "form__removeInput"
+    removeInput.setAttribute("type","button")
+    inputContianer.appendChild(removeInput)
+    removeInput.addEventListener("click", removeNewInput)
+  }
+  event.preventDefault()
+}
+
+
+function removeNewInput() {
+  for (let index = 0; index < 2; index++) {
+    inputContianer.removeChild(inputContianer.lastChild)
+  }
+  
+}
+
+
+newInputCEP.addEventListener("click",createNewInput)
+
+
+
+
