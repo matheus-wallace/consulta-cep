ab142507f6ca8c9369907975ddea3924b20e3502
Matheus Wallace da Silva Pereira <ma.wallace1@hotmail.com>
2022-09-28 23:09:11 -0300

    feat: getting the data and present on screen

--- js/getRequest.js
+++ js/getRequest.js
@@ -1,13 +1,31 @@
 const buttonRequest = document.querySelector(".form__buttonRequest")
-const url = "https://viacep.com.br/"
+const url = "https://viacep.com.br/ws/"
 let ceps = []
+const articlesChild = document.querySelector(".article__result")
 
-function getCEP(event) {
+async function getCEP(event) {
   let inputs = inputContianer.querySelectorAll(".form__inputCEP")
-  inputs.forEach(elements => {
-    ceps.push(elements.value)
-  });
-  
+  for (let index = 0; index < inputs.length; index++) {
+    let cepFormatado = inputs[index].value.replace(".","").replace("-","")
+    ceps.push(cepFormatado)
+    fetch(`${url}${ceps[index]}/json`)
+    .then(response => {return response.json()})
+    .then(data => {
+      const cep = document.createElement("span")
+      const publicplace = document.createElement("span")
+      const district = document.createElement("span")
+      const locale = document.createElement("span")
+      cep.innerHTML = data.cep
+      publicplace.innerHTML = data.logradouro
+      district.innerHTML = data.bairro
+      locale.innerHTML = data.localidade
+      document.querySelector(".result__cep").appendChild(cep)
+      document.querySelector(".result__publicplace").appendChild(publicplace)
+      document.querySelector(".result__district").appendChild(district)
+      document.querySelector(".result__locale").appendChild(locale)
+    })
+    .catch(error => {console.log(error)})
+  }
   event.preventDefault()
 }
 
